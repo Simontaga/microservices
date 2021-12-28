b0f168ba626b0b572c0930162a2feed4fe5e69f8
Simon Törnlund <simon.tornlund@gmail.com>
2021-12-28 14:46:36 +0100

    Add basket service

---
+++ FreakyFashion.BasketService/Controllers/BasketsController.cs
@@ -0,0 +1,47 @@
+﻿using FreakyFashion.BasketService.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FreakyFashion.BasketService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketsController : ControllerBase
+    {
+       
+        public BasketsController(IDistributedCache cache) 
+        {
+            Cache = cache;
+        }
+
+
+        public IDistributedCache Cache { get; }
+
+        [HttpGet("{identifier}")]
+        public ActionResult<BasketDto> Get(string identifier)
+        {
+            var serializedBasket = Cache.GetString(identifier);
+
+            if (serializedBasket == null)
+                return NotFound(); 
+
+            var basketDto = JsonSerializer.Deserialize<BasketDto>(serializedBasket);
+
+            return Ok(basketDto);
+        }
+
+       
+        [HttpPut("{id}")]
+        public IActionResult Put(BasketDto basketdto)
+        {
+            var serializedBasket = JsonSerializer.Serialize(basketdto);
+
+            Cache.SetString(basketdto.Identifier, serializedBasket);
+
+            return Created("", null); // 201 Created
+        }
+
+    }
+}
---
+++ FreakyFashion.BasketService/Models/DTO/BasketDto.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace FreakyFashion.BasketService.Models.DTO
+{
+    public class BasketDto
+    {
+        public string Identifier { get; set; }
+
+        public List<Item> Items { get; set; }
+    }
+
+
+    public class Item
+    {
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
+
+
